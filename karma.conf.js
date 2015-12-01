bc2fa0b04ccbced8f9ba6979d42b37f27590fd31
Daniel Koza <dakolech@gmail.com>
2015-12-01 10:13:29 +0100

    add angular and angular-mock

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.spec.js'
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'test-helpers.js',
+      'test/**/*.js'
     ],
 
 
@@ -60,7 +63,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultanous
