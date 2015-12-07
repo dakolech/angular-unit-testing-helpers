902b8659f18d1a2356cb6111e84dd89d93bbac79
Daniel Koza <dakolech@gmail.com>
2015-12-07 15:03:23 +0100

    add examples to Dummy element

---
+++ examples/Dummy/directive-without-dummy.js
@@ -0,0 +1,51 @@
+function someDirective() {
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    template: '<div another-element></div>'
+  };
+  return directive;
+}
+
+function anotherElement() {
+  var directive = {
+    restrict: 'EA',
+    replace: true,
+    template: '<div></div>'
+  };
+  return directive;
+}
+
+anotherElement.$inject = ['someService'];
+
+angular
+.module('directiveWithoutDummy', [])
+.directive('someDirective', someDirective)
+.directive('anotherElement', anotherElement);
+
+
+describe('someDirective', function() {
+  var
+    element, $compile, $rootScope, $scope;
+
+  beforeEach(module('directiveWithoutDummy', {
+    anotherElementDirective: [{ restrict: 'AE' }]
+  }));
+
+  beforeEach(function() {
+    inject(function(_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope;
+      element = angular.element('<some-directive></some-directive>');
+      $compile(element)($scope);
+      angular.element(document.body).append(element);
+      $scope.$apply();
+    });
+  });
+
+  it('should not be null', function() {
+    expect(element).toBeTruthy();
+  });
+});
+
---
+++ examples/Dummy/filter-without-dummy.js
@@ -0,0 +1,55 @@
+function someDirective() {
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    template: '<div> {{ text | capitalize }}</div>'
+  };
+  return directive;
+}
+
+function capitalize() {
+  var directive = {
+    restrict: 'EA',
+    replace: true,
+    template: '<div></div>'
+  };
+  return directive;
+}
+
+capitalize.$inject = ['someService'];
+
+angular
+.module('filterWithoutDummy', [])
+.directive('someDirective', someDirective)
+.directive('capitalize', capitalize);
+
+
+describe('someDirective', function() {
+  var
+    element, $compile, $rootScope, $scope;
+
+  beforeEach(module('filterWithoutDummy', {
+    capitalizeFilter: function() {
+      return function(input) {
+        return input;
+      };
+    }
+  }));
+
+  beforeEach(function() {
+    inject(function(_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope;
+      element = angular.element('<some-directive></some-directive>');
+      $compile(element)($scope);
+      angular.element(document.body).append(element);
+      $scope.$apply();
+    });
+  });
+
+  it('should not be null', function() {
+    expect(element).toBeTruthy();
+  });
+});
+
