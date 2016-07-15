13995887af68d97abde275c2d3aea8a0e9f90a04
Daniel Koza <dakolech@gmail.com>
2016-07-15 09:20:39 +0200

    add proper exports in index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require('./test-helpers');
+const TestHelpers = require('./test-helpers');
+
+module.exports = {
+    TestServ: TestHelpers.TestServ,
+    TestElement: TestHelpers.TestElement,
+    TestDummy: TestHelpers.TestDummy,
+    TestFactory: TestHelpers.TestFactory,
+    TestModule: TestHelpers.TestModule
+};
+
+if (typeof exports === 'object') {
+  exports.TestServ = TestHelpers.TestServ;
+  exports.TestElement = TestHelpers.TestElement;
+  exports.TestDummy = TestHelpers.TestDummy;
+  exports.TestFactory = TestHelpers.TestFactory;
+  exports.TestModule = TestHelpers.TestModule;
+}
